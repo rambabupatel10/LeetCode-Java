f78003d935ecd11aa70c34b8930bf5d6c8c24a55
Ram Babu Patel <asdram315@gmail.com>
2026-07-24 11:22:21 +0530

    Time: 7 ms (96.06%), Space: 47 MB (36.84%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,134 @@
+class MyLinkedList {
+
+    class Node {
+        int val;
+        Node next;
+        Node prev;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        return temp.val;
+    }
+
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+
+        if (size == 0) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+
+        if (size == 0) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+
+        if (index < 0 || index > size) {
+            return;
+        }
+
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+
+        Node temp = head;
+
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+
+        Node newNode = new Node(val);
+
+        newNode.next = temp.next;
+        newNode.prev = temp;
+
+        temp.next.prev = newNode;
+        temp.next = newNode;
+
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        if (size == 1) {
+            head = tail = null;
+            size--;
+            return;
+        }
+
+        if (index == 0) {
+            head = head.next;
+            head.prev = null;
+            size--;
+            return;
+        }
+
+        if (index == size - 1) {
+            tail = tail.prev;
+            tail.next = null;
+            size--;
+            return;
+        }
+
+        Node temp = head;
+
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+
+        size--;
+    }
+}
