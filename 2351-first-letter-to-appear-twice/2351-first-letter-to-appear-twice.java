eaa3c83e069fdc59dd420a2da7033cc394fa1e37
Ram Babu Patel <asdram315@gmail.com>
2026-08-09 15:55:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (99.06%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        
+    int freq[]=new int [26];
+    for(int i=0;i<s.length();i++){
+        char ch=s.charAt(i);
+        freq[ch-'a']++;
+        if(freq[ch-'a']==2){
+            return ch;
+        }
+    }
+    return 'a';
+        
+    }
+}
