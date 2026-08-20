0a30adbc7a81f401815e4843b72e7f99ba27468f
Ram Babu Patel <asdram315@gmail.com>
2026-08-21 02:39:48 +0530

    Time: 1 ms (89.73%) | Memory: 42.9 MB (15.56%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for(int x=0;x<31;x++){
+            if(x%2==0 && (1<<x)==n){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
