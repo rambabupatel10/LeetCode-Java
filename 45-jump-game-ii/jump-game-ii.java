1b0a315126621e2ebf935b458595ff0b2ca0f016
Ram Babu Patel <asdram315@gmail.com>
2026-09-10 15:57:27 +0530

    Time: 1 ms (99.79%) | Memory: 47.3 MB (38.61%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currEnd = 0;
+        int currMax = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            currMax = Math.max(currMax, i + nums[i]);
+
+            if (i == currEnd) {
+                jumps++;
+                currEnd = currMax;
+            }
+        }
+
+        return jumps;
+    }
+}
