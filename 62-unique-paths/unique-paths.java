7d044332fd05bae37d4d8465da090922421ebb04
Ram Babu Patel <asdram315@gmail.com>
2026-09-15 02:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (81.97%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int total = m + n - 2;
+        int r = Math.min(m - 1, n - 1);
+
+        long ans = 1;
+
+        for (int i = 1; i <= r; i++) {
+            ans = ans * (total - r + i) / i;
+        }
+
+        return (int) ans;
+    }
+}
