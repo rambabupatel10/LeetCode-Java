1282840cb9fcd35775a792388b81916370d202d9
Ram Babu Patel <asdram315@gmail.com>
2026-09-21 00:31:33 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (18.31%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         int sum=0;
         for(int i=0;i<s.length();i++){
             int ch = 'z' - s.charAt(i) + 1;
-           //int prod=ch*(i+1);
+           int prod=ch*(i+1);
            sum+=ch*(i+1);
         }
         return sum;
