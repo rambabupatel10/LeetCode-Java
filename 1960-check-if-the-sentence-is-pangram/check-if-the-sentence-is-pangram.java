bdedc8567e03642e938497817eeef35ddb41af57
Ram Babu Patel <asdram315@gmail.com>
2026-09-24 01:06:23 +0530

    Time: 2 ms (61.75%) | Memory: 42.5 MB (96.15%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        if(sentence.length()<26){
-            return false;
-        }
+        // if(sentence.length()<26){
+        //     return false;
+        // }
         for(char ch = 'a'; ch <= 'z'; ch++) {
             boolean found=false;
         for(int i=0;i<sentence.length();i++){
